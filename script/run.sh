c1878303597eee9a95fdc20daacfc40e6ada7e45
kento-hayakawa <qpwoeiruty2616@yahoo.co.jp>
2019-03-23 22:58:30 +0900

    [add] cron設定用の記載を追加

--- script/run.sh
+++ script/run.sh
@@ -2,10 +2,16 @@
 set -e
 
 function main () {
+  # * 12 * * * /home/[User]/count_traffic_accident/script/run.sh >> /home/[User]/count_traffic_accident/accident.log
+  # 上記のようにcronを設定した場合、ディレクトリの移動が必要
+  cd /home/kento-hayakawa/count_traffic_accident
+
   python3 -m venv venv
   source venv/bin/activate
 
-  pip install -r requirements.txt
+  # cronのログに不要な文字列を出力しないようにする
+  # 初回のみ実行でよい
+  # pip install -r requirements.txt
   
   python3 -m count_traffic_accident
 
